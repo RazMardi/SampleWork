0f3e91c1746794907e75d11c65619d4ddc6157d3
RazMardi <r.mardirossian@digipen.edu>
2016-05-30 02:29:07 -0700

    Adding List Data Structure

---
+++ Data Structures/List.h
@@ -0,0 +1,61 @@
+/*****************************************************************************/
+/*!
+@file   List.h
+@author Razmig Mardirossian
+@par    email: r.mardirossian\@digipen.edu
+@date   5/29/2016
+@brief  contains the interface of the List class
+*/
+/*****************************************************************************/
+#ifndef LIST_H
+#define LIST_H
+#include <iostream> 
+
+namespace DataStructures
+{
+    template <typename Data> class List;
+
+    template <typename Data>
+    std::ostream & operator<<(std::ostream & os, const List<Data> &list);
+
+    template <typename Data>
+    class List
+    {
+    public:
+        List();
+        ~List();
+        List(const List& rhs);
+        List& operator=(const List& rhs);
+
+        bool IsEmpty() const;
+        void PushBack(const Data& value);
+        void PushFront(const Data& value);
+        void PopFront();
+        void PopBack();
+        void Clear();
+        int Find(const Data& value) const;
+        void RemoveNode(const Data& value); 
+        void Reverse();
+        void Insert(unsigned position, const Data& value);
+        const Data& Front() const;
+        const Data& Back() const;
+        unsigned Size() const;
+        friend std::ostream & operator<< <Data>(std::ostream & os, const List &list);
+
+    private:
+        struct Node
+        {
+            Node(Data data);
+            ~Node();
+            Node* next;
+            Node* prev;
+            Data data;
+        };
+        Node *_head; 
+        Node *_tail; 
+        Node *CreateNode(const Data& value) const;
+        mutable unsigned _size;  
+    };
+}
+#endif
+#include "List.cpp"
---
+++ Sorting Algorithms/BubbleSort.cpp
@@ -0,0 +1,64 @@
+/*****************************************************************************/
+/*!
+@file   BubbleSort.cpp
+@author Razmig Mardirossian
+@par    email: r.mardirossian\@digipen.edu
+@date   5/17/2016
+@brief  The Bubble Sort algorithm is best used on small data sets. Since it must
+make multiple passes on the container, it is inefficient on larger data sets.
+A bubble sort works by comparing adjacent items and exchanging them if they 
+are out of order
+*/
+/*****************************************************************************/
+#include <iostream>
+
+/******************************************************************************
+\fn void Swap(T& a, T&b)
+@brief utility function for swapping two built in data types in a container
+@param a the first value being swapped
+@param b the second value being swapped
+*******************************************************************************/
+template <typename T>
+void Swap(T& a, T&b)
+{
+    T temp = b;
+    b = a;
+    a = temp;
+}
+
+/******************************************************************************
+\fn void BubbleSort(T a[], unsigned size)
+@brief sorts an array via the Bubble Sort Algorithm. Time Complexity; 
+Best Case: O(N), Worst Case: O(N^2)
+@param a the array to be sorted
+@param size the size of the array
+*******************************************************************************/
+template <typename T>
+void BubbleSort(T a[], unsigned size)
+{
+    for (int i = 0; i < size - 1; i++) 
+    {
+        unsigned swaps = 0;
+        for (unsigned j = 0; j < size - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                Swap(a[j], a[j + 1]);
+                swaps = 1;
+            }
+        }
+        if (swaps == 0)
+            break;
+    }
+}
+
+int main()
+{
+    int arr[] = { 4, 1, 3, 7, 2, 10 };
+    unsigned size = sizeof(arr) / sizeof(arr[0]);
+    BubbleSort(arr, size);
+    for (unsigned i = 0; i < size; ++i)
+        std::cout << arr[i] << " ";
+    return 0;
+}
+
